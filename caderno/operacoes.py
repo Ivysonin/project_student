4e4a002df9b0dc6763b143fe1343cd1d7ee662e3
ivysonin <ivysonthauan4@gmail.com>
2025-02-10 18:45:04 -0300

    docs: removido instruções de teste.

--- caderno/operacoes.py
+++ caderno/operacoes.py
@@ -18,26 +18,12 @@ def porcentagem(a_d : int, porcentagen: int | float, valor : int | float) -> flo
     elif a_d == '2' : # Desconto
         resultado = valor - (valor * porcentagen/100)
         return resultado
-if __name__ == "__main__" :
-    """Testtando porcentagem"""
-    #escolha = int(input ("Digite '1' para acrescimo '2' para desconto: "))
-    #if escolha == 1 : # Acrescimo
-        #valor = float(input ("Digite um valor: "))
-        #porcentagem_ = float(input ("Digite a porcentagem: "))
-
-    #elif escolha == 2 : # Desconto
-        #valor = float(input ("Digite um valor: "))
-        #porcentagem_ = float(input ("Digite a porcentagem: "))
-#print (f"Diminuindo {porcentagem_}% a {valor} fica {porcentagem(escolha, porcentagem_, valor)}")
 
 
 def tabuada (a : int) -> int :
     for i in range(1, 10+1) :
         resultado = a * i
         print (f"{a} x {i} = {resultado}")
-if __name__ == "__main__" :
-    """Testando tabuada"""
-    #tabuada(-2)
 
 
 raiz_quadrada = lambda a: sqrt(a)
